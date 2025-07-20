52409e1711eb3c314405f6c71ccd8fa306507b78
Adriana E Reyes <ladri8@hotmail.com>
2025-07-19 20:35:45 -0400

    Modified config module

--- src/propval/shared/config.py
+++ src/propval/shared/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
+from typing import List, Dict, Any, Optional
 
 
 class Settings(BaseSettings):
@@ -7,6 +8,25 @@ class Settings(BaseSettings):
     #Data paths
     train_data: str = "data/train.csv"
     test_data: str = "data/test.csv"
+
+    # Feature configs
+    
+    # id or id_col (since id is a python keyword is not present in the data provided)
+    # this can be made extensible in case other data versins have it
+    id_col: Optional[str] = None
+
+
+    categorical_cols: List[str] = ["type", "sector"]
+    target: str = "price"
+
+    # Model hyperparameters
+    model_params: Dict[str, Any] = {
+
+        "learning_rate": 0.01,
+        "n_estimators": 300,
+        "max_depth": 5,
+        "loss": "absolute_error",
+    }
 
     #add model paths here later 
     
